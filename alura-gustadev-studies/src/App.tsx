0c52a26c2c245c189d1a2ac21296f4b719572483
Gustavo Vieira <gustavo.vieira@ituran.com.br>
2025-03-07 00:14:33 -0300

    feat: Criando o componente Formulário.

--- alura-gustadev-studies/src/App.tsx
+++ alura-gustadev-studies/src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Button from "./components/Button/Button";
+import Formulario from "./components/Formulario/Formulario";
 
 function App() {
   return (
     <>
-      <Button />
+      <Formulario />
     </>
   );
 }
