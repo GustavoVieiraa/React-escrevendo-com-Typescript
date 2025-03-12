d4c2df3d6ade044bdd8381ce5ac83745d07753fd
Gustavo Vieira <gustavo.vieira@ituran.com.br>
2025-03-11 21:04:52 -0300

    feat: Finalizado a implementação da estilização utlizando Sass.

---
+++ alura-gustadev-studies/src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Formulario from "../components/Formulario/Formulario";
+import List from "../components/List/List";
+import "../pages/style.scss";
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario />
+      <List />
+    </div>
+  );
+}
+
+export default App;
