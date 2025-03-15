ea62a5a5fec58bd130487d31ec08d20292da1160
Gustavo Vieira <gustavo.vieira@ituran.com.br>
2025-03-15 00:37:37 -0300

    feat: Implementando o uso de State no nosso formulario, para criar novos Item.

--- alura-gustadev-studies/src/components/Formulario/Formulario.tsx
+++ alura-gustadev-studies/src/components/Formulario/Formulario.tsx
@@ -3,15 +3,32 @@ import Button from "../Button/Button";
 import style from "./Formulario.module.scss";
 
 class Formulario extends React.Component {
+  state = {
+    tarefa: "",
+    tempo: "00:00",
+  };
+
+  adicionarTarefa(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("state: ", this.state);
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form
+        className={style.novaTarefa}
+        onSubmit={this.adicionarTarefa.bind(this)}
+      >
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
             name="tarefa"
             id="tarefa"
+            onChange={(event) =>
+              this.setState({ ...this.state, tarefa: event.target.value })
+            }
+            value={this.state.tarefa}
             placeholder="O que você quer estudar"
           />
         </div>
@@ -21,6 +38,10 @@ class Formulario extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            onChange={(event) =>
+              this.setState({ ...this.state, tempo: event.target.value })
+            }
+            value={this.state.tempo}
             id="tempo"
             min="00:00:00"
             max="01:30:00"
